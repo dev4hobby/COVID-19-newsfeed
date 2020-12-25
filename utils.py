43dcf90bcf8b45433d49bb47fd9534e749022c57
Seongchuel <aciddust20@gmail.com>
2020-12-25 15:39:29 +0900

    Revert "Add, hash manager"
    This reverts commit 59ba98ba62ad74267ec52d028745386a961faa31.

--- utils.py
+++ utils.py
@@ -20,22 +20,3 @@ class UrlManager():
     for prefix in self.prefix:
       if _url.startswith(prefix):
         return _url[len(prefix):]
-
-class HashManager():
-  '''
-  for Hash something
-  '''
-  import hmac
-  MASTER_KEY = '07fcc4eddc24f00a85cfd19502b611a4607b8b7e3e361c41170237bc1eaaf2e1'
-  def __init__(self):
-    '''
-    HashManger init method
-    '''
-
-  def hash_hmac(self, algorithm, data, key):
-    res = self.hmac.new(key.encode(), data.encode(), algorithm).hexdigest()
-    return res
-  
-  def generate_hmac_hash(self, source):
-    return self.hash_hmac("sha256", source, self.MASTER_KEY)
-
