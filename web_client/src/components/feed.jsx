804140c447ced4a259f9b258abb794ee1ff2c489
Seongchuel <aciddust20@gmail.com>
2020-12-26 01:57:55 +0900

    design Feed component (card)

--- web_client/src/components/feed.jsx
+++ web_client/src/components/feed.jsx
@@ -1,11 +1,68 @@
-import * as React from 'react'
+import React from 'react'
+import Card from '@material-ui/core/Card';
+// import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+// import Button from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography';
+import { makeStyles } from '@material-ui/core';
 
-const Feed = ({ feed }) => (
-  <div>
-    
-    <p href={feed.link}>{feed.title}</p>
-    <p>{feed.date}</p>
-  </div>
-);
+const useStyles = makeStyles({
+  root: {
+    minWidth: '100%',
+    marginTop: '1vh',
+    marginBottom: '1vh',
+  },
+  bullet: {
+    display: 'inline-block',
+    margin: '0 2px',
+    transform: 'scale(0.8)',
+  },
+  title: {
+    fontSize: 14,
+  },
+  pos: {
+    marginBottom: 12,
+  },
+});
 
-export default Feed
+const checkColor = (title) => {
+  if (title.indexOf('백신') !== -1) {
+    return '#D5F5E3'
+  } else if (title.indexOf('사망') !== -1
+    || title.indexOf('숨져') !== -1) {
+    return '#F5B7B1'
+  } else if (title.indexOf('확진') !== -1
+    || title.indexOf('감염') !== -1
+    || title.indexOf('명째') !== -1) {
+    return '#FCF3CF'
+  }
+  return ''
+}
+
+export default function Feed({ feed }) {
+  const classes = useStyles();
+  const colorString = checkColor(feed.title)
+  return (
+    <Card className={classes.root} style={{
+      backgroundColor: colorString
+    }}>
+      <CardContent>
+        {/* <Typography className={classes.title} color="textSecondary" gutterBottom>
+          Word of the Day
+        </Typography> */}
+        {/* <Typography variant="h5" component="h2">
+          Subject
+        </Typography> */}
+        <Typography className={classes.pos} color="textSecondary">
+          <b>{feed.title}</b>
+        </Typography>
+        <Typography variant="body2" component="p">
+          {feed.date}
+        </Typography>
+      </CardContent>
+      {/* <CardActions>
+        <Button size="small">Learn More</Button>
+      </CardActions> */}
+    </Card>
+  )
+} 
--- web_client/src/pages/index.jsx
+++ web_client/src/pages/index.jsx
@@ -1,22 +1,30 @@
-import * as React from 'react'
+import React from 'react'
+import CssBaseline from '@material-ui/core/CssBaseline';
+import Typography from '@material-ui/core/Typography';
+import Container from '@material-ui/core/Container';
 import Feeds from '../components/feeds.jsx'
-
-const pageStyles = {
-  color: "#232129",
-  padding: "96px",
-  fontFamily: "-apple-system, Roboto, sans-serif, serif",
-}
 
 const feedUrl = 'https://raw.githubusercontent.com/dev4hobby/COVID-19-newsfeed/master/feed.json'
 
 // markup
 const IndexPage = () => {
   return (
-    <main style={pageStyles}>
-      <title>COVID-19 Newsfeed</title>
-      <Feeds feedUrl={feedUrl}> </Feeds>
-    </main>
+    <React.Fragment>
+      <CssBaseline />
+      <Container maxWidth="md">
+        <Typography component="div" style={{
+          // backgroundColor: '#D1F2EB',
+          height: '100vh',
+          paddingTop: '5vh'
+        }}>
+          <h2>COVID-19 Newsfeed</h2>
+          <br></br>
+          <Feeds feedUrl={feedUrl}> </Feeds>
+        </Typography>
+      </Container>
+    </React.Fragment>
   )
 }
+
 
 export default IndexPage
