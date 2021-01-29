4116045feb2ef62354dd059b1fee558624c75d1c
Seongchuel <aciddust20@gmail.com>
2021-01-29 14:24:27 +0900

    revert

--- run.py
+++ run.py
@@ -49,8 +49,7 @@ for element in elements:
   if not feed_data.get(feed_hash) and len(new_feed_data) <= FEED_MAX:
     new_feed_data[feed_hash] = {
       'title': feed_title,
-      # 'link': url_manager.remove_url_prefix(feed_link),
-      'link': feed_link,
+      'link': url_manager.remove_url_prefix(feed_link),
       'date': upload_time
     }
 
